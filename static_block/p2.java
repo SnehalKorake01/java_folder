3eb3bd71b1ffb415b2c4d0c7d877273755107dd4
snehal <snehalkorake5@gmail.com>
2026-05-21 23:53:54 +0530

    added

---
+++ static_block/p2.java
@@ -0,0 +1,10 @@
+class Demo{
+	public static void main(String[]args){
+	}
+	static{
+		System.out.println("in static block 1");
+	}
+	static{
+		System.out.println("in static block 2");
+	}
+}
---
+++ static_block/p3.java
@@ -0,0 +1,9 @@
+class Demo{
+	public static void main(String[]args){
+	}
+		static int a=10;
+		static {
+			System.out.println("in static block:"+a);
+		}
+
+}
