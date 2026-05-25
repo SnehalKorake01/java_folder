a3fde9c4e748f52bb0fb01ad4bdbad9c5187f914
snehal <snehalkorake5@gmail.com>
2026-05-25 23:45:00 +0530

    added

---
+++ Oop/StringBuffer/p1.java
@@ -0,0 +1,6 @@
+class Demo{
+	public static void main(String[]args){
+		StringBuffer s=new StringBuffer("snehal");
+		System.out.println(s);
+	}
+}
---
+++ Oop/StringBuffer/p3.java
@@ -0,0 +1,10 @@
+class Demo{
+	public static void main(String[]args){
+		StringBuffer s=new StringBuffer("snehal");
+		System.out.println(System.identityHashCode(s));
+		s=new StringBuffer("korake");
+		System.out.println(System.identityHashCode(s));
+
+		
+	}
+}
