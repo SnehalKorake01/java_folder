8fd4e07872be01beaac1c07ab0c453dc27f38f60
snehal <snehalkorake5@gmail.com>
2026-05-25 21:56:07 +0530

    added

---
+++ Oop/Strings/p8.java
@@ -0,0 +1,8 @@
+class Demo{
+	public static void main(String[]args){
+		String s=new String("snehal");
+		System.out.println(System.identityHashCode(s));
+		String s1=new String("snehal");
+		System.out.println(System.identityHashCode(s));
+	}
+}s
